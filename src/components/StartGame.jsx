d0338f3353bdfb2bff738723e8d92386a9729fb5
umesh2407 <choudharyumesh986@gmail.com>
2024-04-08 20:50:17 +0530

    added Start Game

---
+++ src/components/StartGame.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import styled from 'styled-components';
+
+const StartGame = () => {
+  return (
+    <Container>
+        <div>
+        <img src='/images/dices.png'/>
+        </div>
+        <div className='content'>
+            <h1>Dice Game</h1>
+            <Button>Play Now</Button>
+        </div>
+    </Container>
+  )
+}
+
+export default StartGame
+
+const Container = styled.div`
+max-width:1180px;
+height: 100vh;
+display: flex;
+margin: 0 auto;
+align-items: center;
+
+.content{
+    h1{
+        font-size: 90px;
+        white-space: nowrap;
+    }
+}
+
+`;
+
+const Button = styled.button`
+color: white;
+padding: 10px 18px;
+background: black;
+border-radius: 5px;
+min-width: 220px;
+border: none;
+font-size: 16px;
+border:1px solid transparent;
+cursor: pointer;
+transition: 0.4s background ease-in ;
+
+
+&:hover{
+    background-color: white;
+    border: 1px solid black;
+    color: black;
+    transition: 0.3s background ease-in ;
+}
+
+`;
